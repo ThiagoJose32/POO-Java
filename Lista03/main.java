1ae8f7256636028acab9b1762835b25e7dc1c3d1
Thiago José <96024152+ThiagoJose32@users.noreply.github.com>
2022-09-26 21:54:18 -0300

    Create main.java

---
+++ Lista03/main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println("Verificador DE PAR OU ÍMPAR");
+//        Lista03_Q01 q1 = new Lista03_Q01();
+//        System.out.print("Digite o valor: ");
+//        Scanner scan = new Scanner(System.in);
+//        int x = scan.nextInt();
+//        q1.setValor(x);
+//        System.out.print("O valor: " + q1.getValor() + " é " + q1.parImpar());
+
+//        System.out.println("Calculadora simples");
+//        Lista03_Q02 q2 = new Lista03_Q02();
+//        int n1, n2;
+//        System.out.print("Digite o primeiro valor: ");
+//        Scanner scan = new Scanner(System.in);
+//        n1 = scan.nextInt();
+//        System.out.print("Digite o segundo valor: ");
+//        n2 = scan.nextInt();
+//        System.out.printf("Resultado = " + q2.menu(n1, n2));
+
+
+    }
+}
