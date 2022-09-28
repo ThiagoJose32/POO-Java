5019f468b7a4b46c41534ab71d2daf444b06b73a
Thiago José <96024152+ThiagoJose32@users.noreply.github.com>
2022-09-27 23:42:01 -0300

    Create Q03.java

---
+++ Lista03/Q03.java
@@ -0,0 +1,21 @@
+import java.lang.String;
+
+public class Q03 {
+    private int qtdVogais;
+
+    public int getQtdVogais(){
+        return qtdVogais;
+    }
+    public void setQtdVogais(int qtdVogais){
+        this.qtdVogais = qtdVogais;
+    }
+
+    public int contaVogais(String[] palavra){
+        int aux;
+
+        for(aux=0;palavra[aux]!="\n";aux++){
+            
+        }
+        return qtdVogais;
+    }
+}
